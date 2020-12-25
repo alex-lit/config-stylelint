98e8ff2f719ba5a8043525c7f90f72bf67c7de14
Alexey Litovchenko <alex.lit@outlook.com>
2020-12-25 15:47:50 +0300

    chore: build

--- scripts/build.sh
+++
@@ -1,2 +0,0 @@
-cross-env OUTPUT_DIR=docs vue-cli-service build
-vue-cli-service build --target lib --name vue-yandex-share ./src/components/vue-yandex-share/index.ts
--- scripts/semver.sh
+++ scripts/semver.sh
@@ -1,5 +1,4 @@
 # Обновляет версию пакета
-npm run build
 git add --all
 git commit -m 'chore: build'
 npm version $1
