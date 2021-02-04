b0d788ac52b8839c348590e644f0106041852be7
Alexey Litovchenko <alex.lit@outlook.com>
2021-02-05 01:34:54 +0300

    chore: preparing for version update

--- plugins/scss.js
+++ plugins/scss.js
@@ -1,4 +1,4 @@
-/* eslint-disable sonarjs/no-duplicate-string */
+const KEBAB_CASE_PATTERN = '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$';
 
 /**
  * @see [stylelint-scss](https://github.com/kristerkari/stylelint-scss)
@@ -8,10 +8,10 @@ module.exports = {
 
   rules: {
     'scss/at-extend-no-missing-placeholder': true,
-    'scss/at-function-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/at-function-pattern': KEBAB_CASE_PATTERN,
     'scss/at-import-no-partial-leading-underscore': true,
     'scss/at-import-partial-extension-blacklist': ['scss'],
-    'scss/at-mixin-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/at-mixin-pattern': KEBAB_CASE_PATTERN,
     'scss/at-rule-no-unknown': [
       true,
       {
@@ -19,11 +19,11 @@ module.exports = {
       },
     ],
     'scss/declaration-nested-properties': 'never',
-    'scss/dollar-variable-pattern': '^[_]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/dollar-variable-pattern': KEBAB_CASE_PATTERN,
     'scss/function-color-relative': true,
     'scss/no-duplicate-dollar-variables': true,
     'scss/no-duplicate-mixins': true,
-    'scss/percent-placeholder-pattern': '^[a-z]+([a-z0-9-]+[a-z0-9]+)?$',
+    'scss/percent-placeholder-pattern': KEBAB_CASE_PATTERN,
     'scss/selector-nest-combinators': 'always',
     'scss/selector-no-redundant-nesting-selector': null,
   },
