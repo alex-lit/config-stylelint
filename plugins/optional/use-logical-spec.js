adc7d49d8883dd1bf9acbf933df1a47e500ca95f
alexlit <alex.lit@outlook.com>
2021-08-04 14:45:21 +0300

    chore: preparing for version update

--- plugins/optional/use-logical-spec.js
+++ plugins/optional/use-logical-spec.js
@@ -5,6 +5,15 @@ module.exports = {
   plugins: ['stylelint-use-logical-spec'],
 
   rules: {
-    'liberty/use-logical-spec': true,
+    'liberty/use-logical-spec': [
+      'always',
+
+      /**
+       * Fixes for mobile safari < 14.7
+       *
+       * @see [caniuse](https://caniuse.com/mdn-css_properties_inset-block-start)
+       */
+      { except: ['top', 'right', 'bottom', 'left'] },
+    ],
   },
 };
