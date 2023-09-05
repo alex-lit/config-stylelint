42c85c052c1aed48a1b47414be8de5306b3b5831
Alexey Litovchenko <alex.lit@outlook.com>
2023-09-05 10:40:53 +0300

    chore: preparing for version update

--- plugins/order.js
+++ plugins/order.js
@@ -24,6 +24,9 @@ module.exports = {
       // CSS custom properties
       'custom-properties',
 
+      // SASS aliases
+      { selector: '%', type: 'rule' },
+
       // Declarations
       'declarations',
 
